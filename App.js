6d1110a909e4dead4d7ab2b8ce88713386123f7c
FeLo9 <153471493+FeLo9@users.noreply.github.com>
2023-12-10 20:37:47 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const charactersContainer = document.getElementById('characters');
+  
+    fetch('https://rickandmortyapi.com/api/character')
+      .then(response => response.json())
+      .then(data => {
+        data.results.forEach(character => {
+          const characterCard = `
+            <div class="col-md-4 mb-3">
+              <div class="card">
+                <img src="${character.image}" class="card-img-top" alt="${character.name}">
+                <div class="card-body">
+                  <h5 class="card-title">${character.name}</h5>
+                  <p class="card-text">Species: ${character.species}</p>
+                  <p class="card-text">Status: ${character.status}</p>
+                </div>
+              </div>
+            </div>
+          `;
+          charactersContainer.innerHTML += characterCard;
+        });
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  });
+  
